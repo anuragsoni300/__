a3554764c9c65acd336ff1a73271a6012f9aa9fd
Anurag Soni <anurag.soni@indiamart.com>
2025-05-20 17:54:47 +0530

    --

--- rotate.go
+++ rotate.go
@@ -3,12 +3,30 @@ package main
 import "fmt"
 
 func RotateByK() {
-	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	rotate := 9
-	for range rotate {
-		temp := a[0]
-		a = a[1:len(a)]
-		a = append(a, temp)
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	rotate := 1
+	rotate %= len(a)
+	b := append(a[rotate:], a[:rotate]...)
+	c := append(a[len(a)-rotate:], a[:len(a)-rotate]...)
+	fmt.Printf("%-30s %v\n", "Rotate by K left", b)
+	fmt.Printf("%-30s %v\n", "Rotate by K right", c)
+
+	reverse(a, 0, len(a)-1)
+	reverse(a, 0, rotate-1)
+	reverse(a, rotate, len(a)-1)
+	fmt.Printf("%-30s %v\n", "Rotate by K right O(1)", a)
+
+	a = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	reverse(a, 0, len(a)-1)
+	reverse(a, 0, len(a)-rotate-1)
+	reverse(a, len(a)-rotate, len(a)-1)
+	fmt.Printf("%-30s %v\n", "Rotate by K left O(1)", a)
+}
+
+func reverse(a []int, s, e int) {
+	for s < e {
+		a[s], a[e] = a[e], a[s]
+		s++
+		e--
 	}
-	fmt.Printf("%-30s %v\n", "Selection Sort", a)
 }
