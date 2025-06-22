9cad3cb4231b314e5dac901cdcc7507604ac99a4
Anurag Soni <anurag.soni@indiamart.com>
2025-06-22 15:55:03 +0530

    --

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ GO := $(shell which go)
 .PHONY: run
 
 run:
-	$(GO) run .
+	@$(GO) run .
 
--- linkedlist/0. basics.go
+++ linkedlist/0. basics.go
@@ -1,10 +1,43 @@
 package linkedlist
 
-type LL struct {
+import "fmt"
+
+type Node struct {
 	Val  int
-	Node *LL
+	Next *Node
+}
+
+type Linklist struct {
+	head *Node
+}
+
+func (list *Linklist) _insert(val int) {
+	newNode := &Node{Val: val}
+	if list.head == nil {
+		list.head = newNode
+	} else {
+		curr := list.head
+		for curr.Next != nil {
+			curr = curr.Next
+		}
+		curr.Next = newNode
+	}
+}
+
+func (list *Linklist) _print() {
+	curr := list.head
+	for curr != nil {
+		fmt.Printf("%v->", curr.Val)
+		curr = curr.Next
+	}
 }
 
 func Linkedlist() {
+	myList := Linklist{}
+	myList._insert(1)
+	myList._insert(2)
+	myList._insert(3)
+	myList._insert(4)
 
+	myList._print()
 }
