2b65b9a058ace26cefe886efc48aaeec1819a12a
Anurag Soni <anurag.soni@indiamart.com>
2025-06-29 19:27:05 +0530

    --

--- linkedlist/0. basics.go
+++ linkedlist/0. basics.go
@@ -116,6 +116,22 @@ func (list *Linklist) _length() {
 	fmt.Printf("%-60s %v\n", "Length of thr linked list", count)
 }
 
+func (list1 *Node) _mergeSortedRec(list2 *Node) *Node {
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	}
+
+	if list1.Val <= list2.Val {
+		list1.Next = list1.Next._mergeSortedRec(list2)
+		return list1
+	} else {
+		list2.Next = list2.Next._mergeSortedRec(list1)
+		return list2
+	}
+}
+
 func (list *Linklist) _print() {
 	curr := list.head
 	for curr != nil {
@@ -126,32 +142,39 @@ func (list *Linklist) _print() {
 }
 
 func Linkedlist() {
-	myList := Linklist{}
+	myList, list2 := Linklist{}, Linklist{}
 	myList._insert(1)
-	myList._insert(2)
+	// myList._insert(2)
 	myList._insert(3)
-	myList._insert(4)
+	// myList._insert(4)
 	myList._insert(5)
-	myList._insert(6)
+	// myList._insert(6)
 	myList._insert(7)
-	myList._insert(8)
+	// myList._insert(8)
 	myList._insert(9)
-	myList._insert(10)
+	// myList._insert(10)
 
+	list2._insert(2)
+	list2._insert(4)
+	list2._insert(6)
+	list2._insert(8)
+	list2._insert(10)
 	// myList._insertAfter(2, 5)
 	// myList._delete(3)
 	// myList._print()
 	// myList.head = myList.head._reverseRec()
 	// myList._reverse()
-	myList._print()
+	// myList._print()
 	// myList._length()
 	// myList._middle()
 
-	curr := myList.head
-	for curr.Next != nil {
-		curr = curr.Next
-	}
-	curr.Next = myList.head.Next
-	myList._detectcycle()
+	// curr := myList.head
+	// for curr.Next != nil {
+	// 	curr = curr.Next
+	// }
+	// curr.Next = myList.head.Next
+	// myList._detectcycle()
+	// myList._print()
+	myList.head = myList.head._mergeSortedRec(list2.head)
 	myList._print()
 }
